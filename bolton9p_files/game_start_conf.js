8a215354b763b3f25f254138f7df6581ccd40702
Dennis Yau <denn.yau@gmail.com>
2016-12-01 23:33:00 -0500

    Figured out how to insert cards.  Added starting game state file.
    The game state "conf" variable stores the location of the House Deck card images.
    The inital game state is kept in a base64 encoded json string.
    
    Once the structure of the game state was known, replaced the starting hash with
    a plaintext string.

---
+++ bolton9p_files/game_start_conf.js
@@ -0,0 +1,77 @@
+default_game_json = {
+    "wildlings": "0",
+    "round": "7",
+    "ironThroneOrder": "Martell\nStark\nTyrell\nBaratheon\nGreyjoy\nLannister\nArryn\nTargaryen\nTully",
+    "fiefdomOrder": "Lannister\nTargaryen\nTyrell\nMartell\nGreyjoy\nTully\nStark\nArryn\nBaratheon ",
+    "kingsCourtOrder": "Stark\nTargaryen\nTully\nTyrell\nBaratheon\nMartell\nGreyjoy\nLannister\nArryn",
+    "garrisons": "The Eyrie: 2\nWinterfell: 2\nCasterly Rock: 2\nHighgarden: 2\nSunspear: 2\nPyke: 2\nRiverrun: 2\nPentos: 2\nDragonstone: 2",
+    "supply": "Arryn: 2\nBaratheon: 3\nGreyjoy: 3\nLannister: 2\nMartell: 3\nStark: 4\nTargaryen: 4\nTully: 3\nTyrell: 4",
+    "victory": "Arryn: 3\nBaratheon: 3\nGreyjoy: 1\nLannister: 3\nMartell: 3\nStark: 4\nTargaryen: 4\nTully: 4\nTyrell: 3",
+    "units": {
+        "arryn": "Eyrie: kn\nBloody Gate: KN\nKing's Road: FM\nThe Narrow Sea: sh, SH, SH\nThe Fingers: FM\nBay of Crabs: SH\nthe vale: KN, SE\nGulltown - Port: Sh\nGulltown: SE, FM",
+        "baratheon": "King's Landing: FM, KN\nCrackclaw point: KN\nStorm's End: KN, KN\nBitterbridge: FM\nBlackwater Bay: SH, SH, SH\nShipbreaker Bay: SH, SH\nDragonstone - Port: SH\nDragonstone: FM",
+        "greyjoy": "Ironman's Bay: sh, SH\nPyke: Footman, FM, FM\nGreywater Watch: KN, KN\nPyke - port: SH",
+        "lannister": "Casterly Rock: SE, SE\nBanefort: KN\nLannisport: KN, KN\nSunset Sea: SH, sh\nGolden Tooth: KN\nLannisport-Harbor: sh\nSearoad Marches: KN",
+        "martell": "Sunspear: SE\nSunspear - port: sh\nRainwood: KN, SE, KN\nBoneway: fm\nPrinces Pass: fm\nBay of Rhoyne: Sh\nSea of Dorne: SH, SH\nEast Summer Sea: SH, SH\nStarfall: FM, KN\nSalt Shore: KN\nLys: fm\nYronwood: KN",
+        "stark": "Flint's Finger: KN, KN\nDeepwood - port: sh\nDeepwood: FM\nMoat Cailin: KN\nKarhold: fm\nThe Shivering Sea: sh\nThe Byte: SH, sh, SH\nBay of Ice: SH\nStoney Shore: FM, FM\nWhite Harbor: KN, KN, SE\nWinterfell: SE\nCastle Black: Fm",
+        "targaryen": "Disputed lands: kn, Fm\nMyr: Kn\nVolantis: Kn, Kn\nVolantis - Port: Sh\nPentos - Port: Sh\nSea of Myrth: routed-Sh, routed-Sh, routed-Sh\nBraavos: Se, Se, Fm\nNorvos: Fm\nPentos: Kn",
+        "tully": "Riverrun: SE, SE\nWhispering Wood: KN\nSeagard: kn, FM\nThe Twins: FM, FM\nSeagard - port: SH\nStoney Sept: KN\nBlackwater: KN, KN, FM",
+        "tyrell": "The Reach: KN, routed-KN\nHighgarden: FM, KN, KN\nOldtown: FM, SE, KN\nOldtown - port: SH\nRedwyne straights: SH\nThe Dornish Marches: fm\nWest Summer Sea: sh, sh\nThe Arbor: fm"
+    },
+    "orders": {
+        "arryn": "Gulltown: M+0\nThe Eyrie: CP\nThe Bloody Gate: CP\nThe Kingsroad: S\nTHe Vale: S",
+        "baratheon": "Dragonstone: CP\nDragonstone - port: CP\nKing's Landing: M+0\nStorm's End: M+1*\nCrackclaw Point: S\nBlackwater Bay: S+1*",
+        "greyjoy": "Pyke: M-1\nPyke - Port: CP\nIronman's Bay: S",
+        "lannister": "Casterly Rock: CP\nSunset Sea: M+0\nGolden Tooth: CP",
+        "martell": "Sea of Dorne: s+1\nYronwood: S\nSalt Shore: S\nLys: CP\nSunspear - port: CP",
+        "stark": "Castle Black: CP\nKarhold: CP\nDeepwood: CP*\nStoney Shore: M+1*\nWhite Harbor: M+0\nFlints Finger: S\nThe Byte: S+1*",
+        "targaryen": "Pentos: S\nPentos - Port: M+0\nVolantis: M-1\nVolantis - Port: S\nMyr: CP*\nDisputed Lands: CP",
+        "tully": "Riverrun: M-1\nWhispering Wood: M+1\nStoney Sept: CP\nSeagard: S+1",
+        "tyrell": "The Arbor: CP\nHighgarden: S*\nOldtown - port: CP\nThe Reach: S\nOldtown: M-1\nWest Summer Sea: S"
+    },
+    "powertokens": {
+        "arryn": "Gulltown\nThe Vale",
+        "baratheon": "Dragonstone",
+        "greyjoy": "",
+        "lannister": "",
+        "martell": "Salt Shore\nYronwood",
+        "stark": "White Harbor\nDreadfort\nDeepwood",
+        "targaryen": "Golden Fields\nAndalos",
+        "tully": "Whispering Wood",
+        "tyrell": "Oldtown\nThree Towers\nThe reach"
+    },
+    "housecards": {
+        "arryn": "<img src=\"bolton9p_files/cards_bolton9p_files/arryn/bronzeyohn.jpg\" width=\"23%\"/>\n<img src=\"bolton9p_files/cards_bolton9p_files/arryn/vardis.jpg\" width=\"23%\"/>\n<img src=\"bolton9p_files/cards_bolton9p_files/arryn/jonarryn.jpg\" width=\"23%\"/>\n<img src=\"bolton9p_files/cards_bolton9p_files/arryn/littlefinger.jpg\" width=\"23%\"/>\n<img src=\"bolton9p_files/cards_bolton9p_files/arryn/robin.jpg\" width=\"23%\"/>\n<img src=\"bolton9p_files/cards_bolton9p_files/arryn/lisaarryn.jpg\" width=\"23%\"/>\n<img src=\"bolton9p_files/cards_bolton9p_files/arryn/sansa.jpg\" width=\"23%\"/>",
+        "baratheon": "<img src=\"bolton9p_files/cards_bolton9p_files/baratheon/stannis.jpg\" width=\"23%\"/>\n<img src=\"bolton9p_files/cards_bolton9p_files/baratheon/snow.jpg\" width=\"23%\"/>\n<img src=\"bolton9p_files/cards_bolton9p_files/baratheon/davos.jpg\" width=\"23%\"/>\n<img src=\"bolton9p_files/cards_bolton9p_files/baratheon/florent.jpg\" width=\"23%\"/>\n<img src=\"bolton9p_files/cards_bolton9p_files/baratheon/salladhor.jpg\" width=\"23%\"/>\n<img src=\"bolton9p_files/cards_bolton9p_files/baratheon/mance.jpg\" width=\"23%\"/>\n<img src=\"bolton9p_files/cards_bolton9p_files/baratheon/melisandre.jpg\" width=\"23%\"/>",
+        "greyjoy": "<img src=\"bolton9p_files/cards_bolton9p_files/greyjoy/euron.jpg\" width=\"23%\"/>\n<img src=\"bolton9p_files/cards_bolton9p_files/greyjoy/victarion.jpg\" width=\"23%\"/>\n<img src=\"bolton9p_files/cards_bolton9p_files/greyjoy/theon.jpg\" width=\"23%\"/>\n<img src=\"bolton9p_files/cards_bolton9p_files/greyjoy/cleftjaw.jpg\" width=\"23%\"/>\n<img src=\"bolton9p_files/cards_bolton9p_files/greyjoy/balon.jpg\" width=\"23%\"/>\n<img src=\"bolton9p_files/cards_bolton9p_files/greyjoy/yara.jpg\" width=\"23%\"/>\n<img src=\"bolton9p_files/cards_bolton9p_files/greyjoy/damphair.jpg\" width=\"23%\"/>",
+        "lannister": "<img src=\"bolton9p_files/cards_bolton9p_files/lannister/jaime.jpg\" width=\"23%\"/>\n<img src=\"bolton9p_files/cards_bolton9p_files/lannister/mountain.jpg\" width=\"23%\"/>\n<img src=\"bolton9p_files/cards_bolton9p_files/lannister/hound.jpg\" width=\"23%\"/>\n<img src=\"bolton9p_files/cards_bolton9p_files/lannister/kevan.jpg\" width=\"23%\"/>\n<img src=\"bolton9p_files/cards_bolton9p_files/lannister/meryntrant.jpg\" width=\"23%\"/>\n<img src=\"bolton9p_files/cards_bolton9p_files/lannister/cersei.jpg\" width=\"23%\"/>\n<img src=\"bolton9p_files/cards_bolton9p_files/lannister/tywin.jpg\" width=\"23%\"/>",
+        "martell": "<img src=\"bolton9p_files/cards_bolton9p_files/martell/oberyn.jpg\" width=\"23%\"/>\n<img src=\"bolton9p_files/cards_bolton9p_files/martell/hotah.jpg\" width=\"23%\"/>\n<img src=\"bolton9p_files/cards_bolton9p_files/martell/obara.jpg\" width=\"23%\"/>\n<img src=\"bolton9p_files/cards_bolton9p_files/martell/tyene.jpg\" width=\"23%\"/>\n<img src=\"bolton9p_files/cards_bolton9p_files/martell/nymeria.jpg\" width=\"23%\"/>\n<img src=\"bolton9p_files/cards_bolton9p_files/martell/ellaria.jpg\" width=\"23%\"/>\n<img src=\"bolton9p_files/cards_bolton9p_files/martell/doran.jpg\" width=\"23%\"/>",
+        "stark": "<img src=\"bolton9p_files/cards_bolton9p_files/stark/nedstark.jpg\" width=\"23%\"/>\n<img src=\"bolton9p_files/cards_bolton9p_files/stark/robb.jpg\" width=\"23%\"/>\n<img src=\"bolton9p_files/cards_bolton9p_files/stark/karstark.jpg\" width=\"23%\"/>\n<img src=\"bolton9p_files/cards_bolton9p_files/stark/greatjon.jpg\" width=\"23%\"/>\n<img src=\"bolton9p_files/cards_bolton9p_files/stark/cassel.jpg\" width=\"23%\"/>\n<img src=\"bolton9p_files/cards_bolton9p_files/stark/arya.jpg\" width=\"23%\"/>\n<img src=\"bolton9p_files/cards_bolton9p_files/stark/bran.jpg\" width=\"23%\"/>",
+        "targaryen": "<img src=\"bolton9p_files/cards_bolton9p_files/targaryen/daenerys.jpg\" width=\"23%\"/>\n<img src=\"bolton9p_files/cards_bolton9p_files/targaryen/barristan.jpg\" width=\"23%\"/>\n<img src=\"bolton9p_files/cards_bolton9p_files/targaryen/daario.jpg\" width=\"23%\"/>\n<img src=\"bolton9p_files/cards_bolton9p_files/targaryen/jorah.jpg\" width=\"23%\"/>\n<img src=\"bolton9p_files/cards_bolton9p_files/targaryen/greyworm.jpg\" width=\"23%\"/>\n<img src=\"bolton9p_files/cards_bolton9p_files/targaryen/tyrion.jpg\" width=\"23%\"/>\n<img src=\"bolton9p_files/cards_bolton9p_files/targaryen/varys.jpg\" width=\"23%\"/>",
+        "tully": "<img src=\"bolton9p_files/cards_bolton9p_files/tully/hoster.jpg\" width=\"23%\"/>\n<img src=\"bolton9p_files/cards_bolton9p_files/tully/edmure.jpg\" width=\"23%\"/>\n<img src=\"bolton9p_files/cards_bolton9p_files/tully/mallister.jpg\" width=\"23%\"/>\n<img src=\"bolton9p_files/cards_bolton9p_files/tully/vance.jpg\" width=\"23%\"/>\n<img src=\"bolton9p_files/cards_bolton9p_files/tully/blackfish.jpg\" width=\"23%\"/>\n<img src=\"bolton9p_files/cards_bolton9p_files/tully/catelyn.jpg\" width=\"23%\"/>\n<img src=\"bolton9p_files/cards_bolton9p_files/tully/frey.jpg\" width=\"23%\"/>",
+        "tyrell": "<img src=\"bolton9p_files/cards_bolton9p_files/tyrell/renly.jpg\" width=\"23%\"/>\n<img src=\"bolton9p_files/cards_bolton9p_files/tyrell/loras.jpg\" width=\"23%\"/>\n<img src=\"bolton9p_files/cards_bolton9p_files/tyrell/garlan.jpg\" width=\"23%\"/>\n<img src=\"bolton9p_files/cards_bolton9p_files/tyrell/brienne.jpg\" width=\"23%\"/>\n<img src=\"bolton9p_files/cards_bolton9p_files/tyrell/mace.jpg\" width=\"23%\"/>\n<img src=\"bolton9p_files/cards_bolton9p_files/tyrell/margaery.jpg\" width=\"23%\"/>\n<img src=\"bolton9p_files/cards_bolton9p_files/tyrell/ladyolenna.jpg\" width=\"23%\"/>"
+    },
+    "housecardTracking": {
+        "arryn": [false, false, false, false, false, false, false],
+        "baratheon": [false, false, false, false, false, false, false],
+        "greyjoy": [false, false, false, false, false, false, false],
+        "lannister": [false, false, false, false, false, false, false],
+        "martell": [false, false, false, false, false, false, false],
+        "stark": [false, false, false, false, false, false, false],
+        "targaryen": [false, false, false, false, false, false, false],
+        "tully": [false, false, false, false, false, false, false],
+        "tyrell": [false, false, false, false, false, false, false]
+    },
+    "availablePowertokens": {
+        "arryn": "5",
+        "baratheon": "5",
+        "greyjoy": "5",
+        "lannister": "5",
+        "martell": "5",
+        "stark": "5",
+        "targaryen": "5",
+        "tully": "5",
+        "tyrell": "5"
+    },
+    "maxPowertokens": "20"
+}
